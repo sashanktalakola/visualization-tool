30f3fff52777857e326dd50a6117c950d3f0f6cf
sashank <sashanktalakola2@gmail.com>
2024-06-08 23:00:35 -0400

    multiple visualizations functionality added

--- app.py
+++ app.py
@@ -3,7 +3,12 @@ import pandas as pd
 from utils.plotting.bar_chart import plot_bar_chart
 from streamlit_echarts import st_pyecharts
 
-PAGE_VISUALIZATIONS = []
+
+if "visualizations" not in st.session_state:
+    st.session_state["visualizations"] = {}
+if "visualizations_counter" not in st.session_state:
+    st.session_state["visualizations_counter"] = 0
+
 
 uploaded_file = st.file_uploader(label="File Upload",
                                  type="csv",
@@ -46,18 +51,22 @@ with st.sidebar:
             submitted = st.form_submit_button()
             if submitted:
                 if visualization_type == "Bar Chart":
-                    PAGE_VISUALIZATIONS.append(plot_bar_chart(
+                    visualization = plot_bar_chart(
                         x_axis_values = list(df[x_axis_column]),
                         y_axis_values = list(df[y_axis_column]),
                         x_label = x_axis_column,
                         y_label = y_axis_column,
                         title = "Top cloud providers 2018",
                         sub_title = "2017-2018 Revenue"
-                    ))
+                    )
+                    st.session_state["visualizations_counter"] += 1
+                    st.session_state["visualizations"][st.session_state["visualizations_counter"]] = visualization
+
 
 st.markdown("# Visualizations")
-if len(PAGE_VISUALIZATIONS) != 0:
-    for visualization in PAGE_VISUALIZATIONS:
-        st_pyecharts(visualization)
+if len(st.session_state["visualizations"]) != 0:
+    for id, visualization_object in st.session_state["visualizations"].items():
+        with st.container(border=True):
+            st_pyecharts(visualization_object, key=f"vis{id}")
 else:
     st.info("Currently no visualizations to display")
