0691759c1f6551515d32729f620d199d6ad4c3f5
sashank <sashanktalakola2@gmail.com>
2024-06-10 22:36:10 -0400

    added box plot functionality - extra variable plotting (by color)

--- utils/plotting/box_plot.py
+++ utils/plotting/box_plot.py
@@ -3,6 +3,16 @@ from pyecharts import options as opts
 from pyecharts.charts import Boxplot
 
 
+def process_values(df, x_column, y_column):
+    x_axis_values = df[x_column].unique().tolist()
+    y_axis_values = []
+    for x_axis_value in x_axis_values:
+        y_axis_value = df[df[x_column] == x_axis_value][y_column].to_list()
+        y_axis_values.append(y_axis_value)
+
+    return x_axis_values, y_axis_values
+
+
 def create_box_plot(
         x_axis_values,
         y_axis_values,
@@ -13,7 +23,7 @@ def create_box_plot(
     
     c = Boxplot()
     c.add_xaxis(x_axis_values)
-    c.add_yaxis(y_label, y_axis_values)
+    c.add_yaxis(y_label, c.prepare_data(y_axis_values))
     c.set_global_opts(
             title_opts=opts.TitleOpts(title=title, subtitle=sub_title),
             xaxis_opts=opts.AxisOpts(name=x_label)
@@ -21,20 +31,54 @@ def create_box_plot(
 
     return c
 
-def plot_box_plot(df, x_column, y_column, x_label, y_label, title, sub_title):
+def create_box_plot_by_color(
+        x_axis_values,
+        y_axis_values,
+        x_label,
+        y_label,
+        title,
+        sub_title,
+        unique_color_col_vals):
+    
+    c = Boxplot()
+    c.add_xaxis(x_axis_values)
+    c.set_global_opts(
+            title_opts=opts.TitleOpts(title=title, subtitle=sub_title),
+            xaxis_opts=opts.AxisOpts(name=x_label),
+            yaxis_opts=opts.AxisOpts(name=y_label)
+    )
+
+    for label, y_axis_value in zip(unique_color_col_vals, y_axis_values):
+        c = c.add_yaxis(label, c.prepare_data(y_axis_value))
+
+    return c
+
+def plot_box_plot(df, x_column, y_column, x_label, y_label, title, sub_title, color):
     if x_label == "": x_label = x_column
     if y_label == "": y_label = y_column
 
     if (df[y_column].dtype == float) or (df[y_column].dtype == int):
+
+        if color is None:
         
-        x_axis_values = df[x_column].unique().tolist()
-        y_axis_values = []
-        for x_axis_value in x_axis_values:
-            y_axis_value = df[df[x_column] == x_axis_value][y_column].to_list()
-            y_axis_values.append(y_axis_value)
+            df = df.dropna(subset=[x_column, y_column])
+            x_axis_values, y_axis_values = process_values(df, x_column, y_column)
+            box_plot = create_box_plot(x_axis_values, y_axis_values, x_label, y_label, title, sub_title)
+            
+            return box_plot
         
-        box_plot = create_box_plot(x_axis_values, y_axis_values, x_label, y_label, title, sub_title)
-        return box_plot
+        else:
+
+            df = df.dropna(subset=[x_column, y_column, color])
+            unique_color_col_vals = df[color].unique()
+            df_subsets = [df[df[color] == item] for item in unique_color_col_vals]
+
+            x_axis_values = df[x_column].unique().tolist()
+            y_axis_values = [process_values(df_subset, x_column, y_column) for df_subset in df_subsets]
+            box_plot = create_box_plot_by_color(x_axis_values, y_axis_values, x_label, y_label, title, sub_title, unique_color_col_vals)
+
+            return box_plot
+
 
     else:
         error_message = f"Box Plot Y-Axis expects column of type `float` or `int`. But found `{df[y_column].dtype}`"
